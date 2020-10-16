a6b17bec320826ae4c78c8ddb4b7f8672aedd962
Nicolas Germain <ngermain@hihouhou.com>
2020-10-17 00:24:15 +0200

    working method updated

--- lib/huginn_brave_catalog_agent/brave_catalog_agent.rb
+++ lib/huginn_brave_catalog_agent/brave_catalog_agent.rb
@@ -110,6 +110,7 @@ module Agents
 
     form_configurable :expected_receive_period_in_days, type: :string
     form_configurable :changes_only, type: :boolean
+
     def validate_options
       if options.has_key?('changes_only') && boolify(options['changes_only']).nil?
         errors.add(:base, "if provided, changes_only must be true or false")
@@ -121,15 +122,7 @@ module Agents
     end
 
     def working?
-      memory['last_status'].to_i > 0
-
-      return false if recent_error_logs?
-      
-      if interpolated['expected_receive_period_in_days'].present?
-        return false unless last_receive_at && last_receive_at > interpolated['expected_receive_period_in_days'].to_i.days.ago
-      end
-
-      true
+      event_created_within?(options['expected_receive_period_in_days']) && !recent_error_logs?
     end
 
     def check
