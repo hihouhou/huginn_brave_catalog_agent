243fdcb72918874d6771ae573a1be94fee099213
Nicolas Germain <ngermain@hihouhou.com>
2022-08-09 19:10:41 +0200

    fix about changed url

--- lib/huginn_brave_catalog_agent/brave_catalog_agent.rb
+++ lib/huginn_brave_catalog_agent/brave_catalog_agent.rb
@@ -141,7 +141,7 @@ module Agents
     private
 
     def fetch
-      uri = URI.parse("https://ads-serve.brave.com/v9/catalog")
+      uri = URI.parse("https://ads-static.brave.com/v9/catalog")
       response = Net::HTTP.get_response(uri)
 
       log "request  status : #{response.code}"
